76e2cf4d4176bf613f7ba230e16acbf8604fb089
Arvi12345678 <118266759+Arvi12345678@users.noreply.github.com>
2022-11-17 18:47:31 +0530

    Update Ar.java

--- Git_hub/src/New/Ar.java
+++ Git_hub/src/New/Ar.java
@@ -11,6 +11,10 @@ public class Ar {
 	System.out.println("Hello this is m1");
 	System.out.println("Hello this is m2");
 	}
+	public void purposeChanges()
+	{
+	System.out.println("Changes")
+	}
 	
 
 }
